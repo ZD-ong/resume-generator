b0e29d275f6001ec6e67ff3bc93ec651c5948771
ZD-ong <384474574@qq.com>
2018-05-05 23:53:51 +0800

    项目初始化结构

---
+++ src/js/av.js
@@ -0,0 +1,15 @@
+var APP_ID = 'BzVuUYN3PncYr1M9s3OjdU9V-gzGzoHsz';
+var APP_KEY = 'CljMGSwyXKYfplnH1gIwxQiB';
+
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+});
+
+var TestObject = AV.Object.extend('TestObject');
+var testObject = new TestObject();
+testObject.save({
+    words: 'Hello World!'
+}).then(function(object) {
+    alert('LeanCloud Rocks!');
+})
