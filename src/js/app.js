5dc4d0e9399b1aec98ab8074b89598cd36cb0e95
ZD-ong <384474574@qq.com>
2018-05-07 13:00:07 +0800

    完成注册登录与后台的交互及保存用户更新

--- src/js/app.js
+++ src/js/app.js
@@ -10,22 +10,62 @@ var app = new Vue({
             jobTitle: '前端工程师',
             phone: '152xxxxxxxx',
             email: 'example@example.com'
+        },
+        login: {
+            email: '',
+            password: ''
+        },
+        signUp: {
+            email:'',
+            password: ''
         }
     },
     methods: {
         onEdit(key,value){
             this.resume[key] = value
         },
+        onLogin(){
+            AV.User.logIn(this.login.email, this.login.password).then(function (user) {
+                console.log(user)
+            }, function (error) {
+                if(error.code === 211){
+                    alert('邮箱不存在')
+                }else if(error.code === 210){
+                    alert('邮箱和密码不匹配')
+                }
+            });
+        },
+        onSignUp(e){
+            // 新建 AVUser 对象实例
+            const user = new AV.User();
+            // 设置用户名
+            user.setUsername(this.signUp.email);
+            // 设置密码
+            user.setPassword(this.signUp.password);
+            // 设置邮箱
+            user.setEmail(this.signUp.email);
+            user.signUp().then(function (user) {
+                console.log(user);
+            }, function (error) {
+            });
+        },
         onClickSave(){
             let currentUser = AV.User.current()
+            console.log(currentUser);
             if(!currentUser) {
-                this.showLogin()
+                this.loginVisible = true
             }else{
                 this.saveResume()
             }
         },
         saveResume(){
-
+            let {id} = AV.User.current()
+            // 第一个参数是 className，第二个参数是 objectId
+            let user = AV.Object.createWithoutData('User', id);
+            // 修改属性
+            user.set('resume', this.resume);
+            // 保存到云端
+            user.save();
         }
     }
 })
